16370d081cd3b94795e8ed3be0dd48745d7e0486
Peter Weightman <peter.w570@gmail.com>
2022-02-18 20:59:03 +0000

    change styling tweaks, move things around, add copyright

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,10 @@
 	export const prerender = true;
 </script>
 
+<script>
+	let year = new Date().getFullYear();
+</script>
+
 <div class="home-hero-container">
 	<picture>
 		<source src="/leia-hero.webp" type="image/webp" />
@@ -99,64 +103,75 @@
 			</div>
 		</li>
 	</ul>
+	<div class="prose">
+		<p>
+			Cambridge Disc Golf club welcomes all players of all abilities, no
+			experience necessary!
+		</p>
+		<p>
+			There are no permanent courses in/around Cambridge, however we have two
+			temporary 18-hole courses mapped out (one short, one long) in Papworth
+			which just requires someone with a basket.
+		</p>
+		<p>
+			We organise club rounds most Sunday's starting at 11am, and will organise
+			them in our
+			<a href="https://www.facebook.com/groups/115658245890790"
+				>Facebook Group</a
+			>. These are the perfect opportunities for beginners, or players new to
+			the area to be shown around the course and/or get advice. If you don't
+			have any discs, let us know and you should be able to borrow some for the
+			round.
+		</p>
+		<p>
+			Occasionally we'll hold tournaments where scores are adjusted by their
+			handicap, and we have club bag tags which are up for grabs whenever club
+			members play on a card together.
+		</p>
+	</div>
 	<img
 		class="jumper-round-img"
 		src="/christmas-round.jpg"
 		alt="Club members looking happy after a christmas jumper round"
 	/>
-	<p>
-		Cambridge Disc Golf club welcomes all players of all abilities, no
-		experience necessary!
-	</p>
-	<p>
-		There are no permanent courses in/around Cambridge, however we have two
-		temporary 18-hole courses mapped out (one short, one long) in Papworth which
-		just requires someone with a basket.
-	</p>
-	<p>
-		We organise club rounds most Sunday's starting at 11am, and will organise
-		them in our
-		<a href="https://www.facebook.com/groups/115658245890790">Facebook Group</a
-		>. These are the perfect opportunities for beginners, or players new to the
-		area to be shown around the course and/or get advice. If you don't have any
-		discs, let us know and you should be able to borrow some for the round.
-	</p>
-	<p>
-		Occasionally we'll hold tournaments where scores are adjusted by their
-		handicap, and we have club bag tags which are up for grabs whenever club
-		members play on a card together.
-	</p>
-	<picture>
+	<!-- <picture>
 		<source src="/course.avif" type="image/avif" />
 		<source src="/course.webp" type="image/webp" />
 		<source src="/course.gif" type="image/gif" />
 		<img src="/course.png" alt="Map of the Papworth Disc Golf Course Layout" />
-	</picture>
+	</picture> -->
 </main>
 
 <footer>
 	<div class="container">
-		<div>
-			<p class="heading" id="footer-pages-heading">Pages</p>
-			<ul aria-labelledby="footer-pages-heading">
-				<li><a href="/holes">Hole Descriptions</a></li>
-				<li><a href="/bag-tag">Bag Tag Standings</a></li>
-				<li><a href="/handicaps">Handicaps</a></li>
-			</ul>
+		<div class="footer-links-container">
+			<div>
+				<p class="heading" id="footer-pages-heading">Pages</p>
+				<ul aria-labelledby="footer-pages-heading">
+					<li><a href="/holes">Hole Descriptions</a></li>
+					<li><a href="/bag-tag">Bag Tag Standings</a></li>
+					<li><a href="/handicaps">Handicaps</a></li>
+				</ul>
+			</div>
+			<div>
+				<p class="heading" id="footer-links-heading">Links</p>
+				<ul aria-labelledby="footer-links-heading">
+					<li>
+						<a href="https://www.facebook.com/groups/115658245890790"
+							>Facebook Group</a
+						>
+					</li>
+					<li>
+						<a href="https://udisc.com/courses/papworth-everard-l2bt">UDisc</a>
+					</li>
+					<li>
+						<a href="https://goo.gl/maps/mjkuvnVjncsBhtCe8">Google Maps</a>
+					</li>
+				</ul>
+			</div>
 		</div>
-		<div>
-			<p class="heading" id="footer-links-heading">Links</p>
-			<ul aria-labelledby="footer-links-heading">
-				<li>
-					<a href="https://www.facebook.com/groups/115658245890790"
-						>Facebook Group</a
-					>
-				</li>
-				<li>
-					<a href="https://udisc.com/courses/papworth-everard-l2bt">UDisc</a>
-				</li>
-				<li><a href="https://goo.gl/maps/mjkuvnVjncsBhtCe8">Google Maps</a></li>
-			</ul>
+		<div class="footer-copyright">
+			<span>&copy; {year} Cambridge Disc Golf Club</span>
 		</div>
 	</div>
 </footer>
@@ -193,6 +208,9 @@
 	}
 
 	nav {
+		position: sticky;
+		top: 0;
+		z-index: 1;
 		background-color: var(--secondary-bg-color);
 	}
 	nav ul {
@@ -214,10 +232,13 @@
 	main > img,
 	main > picture img {
 		display: block;
-		margin-block: 2rem;
 		width: 100%;
 		max-width: 45rem;
 		margin-inline: auto;
+	}
+
+	main > * + * {
+		margin-block: 2rem;
 	}
 
 	.heading-cards {
@@ -249,17 +270,22 @@
 		font-size: 1.1em;
 	}
 	footer .container {
-		display: flex;
-		flex-wrap: wrap;
 		padding: 1rem;
 	}
-	footer .container > * {
+	.footer-links-container {
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.footer-links-container > * {
 		margin-inline-end: 2rem;
 	}
-	footer .container ul {
+	.footer-links-container ul {
 		padding-inline-start: 1.5ch;
 	}
-	footer .container ul li {
+	.footer-links-container ul li {
 		margin-block: 1ex;
+	}
+	.footer-copyright {
+		text-align: center;
 	}
 </style>
